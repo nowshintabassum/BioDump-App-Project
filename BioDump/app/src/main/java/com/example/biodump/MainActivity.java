f30ec431f338ba27f30a5caa528fe0adb88983d3
tahiyasafa <47198892+tahiyasafa@users.noreply.github.com>
2021-02-09 10:55:40 +0600

    splash screen and main menu added

--- BioDump/app/src/main/java/com/example/biodump/MainActivity.java
+++ BioDump/app/src/main/java/com/example/biodump/MainActivity.java
@@ -12,62 +12,18 @@ import android.widget.ProgressBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar progress_bar;
-    int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Progress Bar
-
-        progress_bar=findViewById(R.id.progress_bar);
-        final Handler handler =new Handler();
+        Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                if(i<=90)
-                {
-                    progress_bar.setProgress(i);
-                    i++;
-                    handler.postDelayed(this,30);
-                }
-                else{
-                    handler.removeCallbacks(this);
-                }
+                Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                startActivity(intent);
             }
-        },30);
-
-        //initialize and assign variable for the bottom nav
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        //set Home Selected
-        bottomNavigationView.setSelectedItemId(R.id.home);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.alternate:
-                        startActivity(new Intent(getApplicationContext()
-                                ,Alternate.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.tree:
-                        startActivity(new Intent(getApplicationContext()
-                                ,tree.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.recycle:
-                        startActivity(new Intent(getApplicationContext()
-                                ,Recycle.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.home:
-                        return true;
-                }
-                return false;
-            }
-        });
+        }, 2500);
     }
 }
 
