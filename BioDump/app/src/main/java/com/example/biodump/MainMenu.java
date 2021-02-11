31f4d1b020ae0b81e055aa1f3449cabd56d18ed9
tahiyasafa <47198892+tahiyasafa@users.noreply.github.com>
2021-02-11 09:48:54 +0600

    zoom anim added

--- BioDump/app/src/main/java/com/example/biodump/MainMenu.java
+++ BioDump/app/src/main/java/com/example/biodump/MainMenu.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageView
+import android.widget.ImageView;
 
 public class MainMenu extends AppCompatActivity {
     Button signinemail,signinphone,signup;
