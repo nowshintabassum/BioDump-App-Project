d64cc056835d6dd12b90bdfafe75d91c9b24695d
tahiyasafa <47198892+tahiyasafa@users.noreply.github.com>
2021-02-11 20:10:51 +0600

    chooseOne subpage added

---
+++ BioDump/app/src/main/java/com/example/biodump/CitizenRegistration.java
@@ -0,0 +1,14 @@
+package com.example.biodump;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CitizenRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_citizen_registration);
+    }
+}
