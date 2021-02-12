32c07185e5371da15ebd046684f032680fd14e47
Nowshin Tabassum <nowshintabassum888@gmail.com>
2021-02-13 02:01:41 +0600

    Half of Alternate page

--- BioDump/app/src/main/java/com/example/biodump/Alternate.java
+++ BioDump/app/src/main/java/com/example/biodump/Alternate.java
@@ -1,20 +1,66 @@
 package com.example.biodump;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Alternate extends AppCompatActivity {
 
+    ImageView iv;
+    private  static final int GALLERY_REQUEST_CODE =123;
+    Button folder,camera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alternate);
+
+        iv = findViewById(R.id.object_pic);
+        folder = findViewById(R.id.folder2);
+        camera = findViewById(R.id.camera2);
+
+        //Request for camera permission
+        if(ContextCompat.checkSelfPermission(Alternate.this, Manifest.permission.CAMERA)
+        != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(Alternate.this,new String[]{
+                    Manifest.permission.CAMERA
+            },100);
+        }
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent camintent =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(camintent,100);
+            }
+        });
+
+        folder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent , "Pick an image"),GALLERY_REQUEST_CODE);
+
+            }
+        });
 
 
         //initialize and assign variable for the bottom nav
@@ -36,7 +82,7 @@ public class Alternate extends AppCompatActivity {
                         return true;
                     case R.id.home:
                         startActivity(new Intent(getApplicationContext()
-                                ,MainActivity.class));
+                                ,Homepg.class));
                         overridePendingTransition(0,0);
                         return true;
                     case R.id.recycle:
@@ -48,4 +94,20 @@ public class Alternate extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Uri imagedata = data.getData();
+            iv.setImageURI(imagedata);
+        }
+        if(requestCode == 100)
+        {
+            Bitmap bitmap =(Bitmap) data.getExtras().get("data");
+            iv.setImageBitmap(bitmap);
+        }
+    }
+
+
 }
