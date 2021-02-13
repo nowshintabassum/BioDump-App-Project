881f73fa62e283ee9a9e1d4dd62aa9a51b38c24c
Nowshin Tabassum <nowshintabassum888@gmail.com>
2021-02-13 17:09:47 +0600

    Alternate page(Object Detection)

--- BioDump/app/src/main/java/com/example/biodump/Alternate.java
+++ BioDump/app/src/main/java/com/example/biodump/Alternate.java
@@ -17,14 +17,27 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionCloudImageLabelerOptions;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Alternate extends AppCompatActivity {
 
     ImageView iv;
     private  static final int GALLERY_REQUEST_CODE =123;
     Button folder,camera;
+    TextView objtv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +46,7 @@ public class Alternate extends AppCompatActivity {
         iv = findViewById(R.id.object_pic);
         folder = findViewById(R.id.folder2);
         camera = findViewById(R.id.camera2);
+        objtv = findViewById(R.id.objecttv);
 
         //Request for camera permission
         if(ContextCompat.checkSelfPermission(Alternate.this, Manifest.permission.CAMERA)
@@ -50,6 +64,8 @@ public class Alternate extends AppCompatActivity {
                 startActivityForResult(camintent,100);
             }
         });
+
+        //Choose picture form gallery
 
         folder.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,11 +117,106 @@ public class Alternate extends AppCompatActivity {
         if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
             Uri imagedata = data.getData();
             iv.setImageURI(imagedata);
+
+            FirebaseVisionImage image;
+            try {
+                image = FirebaseVisionImage.fromFilePath(getApplicationContext(), imagedata);
+                FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
+                      .getOnDeviceImageLabeler();
+
+           //     Or, to set the minimum confidence required:
+        // FirebaseVisionCloudImageLabelerOptions options =
+        //        new FirebaseVisionCloudImageLabelerOptions.Builder()
+          //           .setConfidenceThreshold(0.7f)
+          //                       .build();
+          //   FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
+          //                   .getCloudImageLabeler();
+
+
+                labeler.processImage(image)
+                        .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+                            @Override
+                            public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                                // Task completed successfully
+                                // ...
+                                double con = 0.00;
+                                objtv.setText("");
+
+
+                                for (FirebaseVisionImageLabel label: labels) {
+                                    String imgtext = label.getText();
+                                    String entityId = label.getEntityId();
+                                    double confidence = label.getConfidence();
+                                    if(confidence>=con)
+                                    {
+                                        objtv.append(imgtext+" \n");
+                                         con = confidence;
+                                    }
+
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                // ...
+                            }
+                        });
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
         }
         if(requestCode == 100)
         {
             Bitmap bitmap =(Bitmap) data.getExtras().get("data");
             iv.setImageBitmap(bitmap);
+            FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+            FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
+                    .getOnDeviceImageLabeler();
+
+            //     Or, to set the minimum confidence required:
+            // FirebaseVisionCloudImageLabelerOptions options =
+            //        new FirebaseVisionCloudImageLabelerOptions.Builder()
+            //           .setConfidenceThreshold(0.7f)
+            //                       .build();
+            //   FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
+            //                   .getCloudImageLabeler();
+
+
+            labeler.processImage(image)
+                    .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+                        @Override
+                        public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                            // Task completed successfully
+                            // ...
+                            double con = 0.00;
+                            objtv.setText("");
+
+
+                            for (FirebaseVisionImageLabel label: labels) {
+                                String imgtext = label.getText();
+                                String entityId = label.getEntityId();
+                                double confidence = label.getConfidence();
+                                if(confidence>=con)
+                                {
+                                    objtv.append(imgtext+" \n");
+                                    con = confidence;
+                                }
+
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Task failed with an exception
+                            // ...
+                        }
+                    });
         }
     }
 
