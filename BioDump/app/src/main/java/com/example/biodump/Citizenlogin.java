b6c9e71c0d97c790ff1bffd13d99ceba725ccd43
tahiyasafa <47198892+tahiyasafa@users.noreply.github.com>
2021-02-15 14:09:03 +0600

    small change

--- BioDump/app/src/main/java/com/example/biodump/Citizenlogin.java
+++ BioDump/app/src/main/java/com/example/biodump/Citizenlogin.java
@@ -2,13 +2,29 @@ package com.example.biodump;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Citizenlogin extends AppCompatActivity {
+    Button btnlogincitizen;
+    EditText emailcitizen,pwdcitizen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citizenlogin);
+        btnlogincitizen =(Button) findViewById(R.id.citizenlogin);
+        btnlogincitizen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent citizenloginemail  = new Intent(Citizenlogin.this,Homepg.class);
+                startActivity(citizenloginemail);
+
+            }
+        });
     }
 }
